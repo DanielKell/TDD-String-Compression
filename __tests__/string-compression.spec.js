f36d23b213939067c21084f6c437b037c5b38a44
DanielKell <danielkell.ubc@gmail.com>
2017-11-15 13:27:43 -0800

    First test written

--- __tests__/string-compression.spec.js
+++ __tests__/string-compression.spec.js
@@ -1,1 +1,11 @@
 const stringCompression = require('../lib/stringCompression.js');
+
+describe('stringCompression', () => {
+
+  describe('when you input a string of letters', () => {
+    it('will return a new string with the number of each character next to it', () => {
+      const result = stringCompression("car");
+      expect(result).toEqual("c1a1r1");
+    });
+  });
+});
