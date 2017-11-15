ac3318699a78594c838f96def7560700751efb7f
DanielKell <danielkell.ubc@gmail.com>
2017-11-15 14:57:54 -0800

    Added new checks for function

--- __tests__/string-compression.spec.js
+++ __tests__/string-compression.spec.js
@@ -4,8 +4,22 @@ describe('stringCompression', () => {
 
   describe('when you input a string of letters', () => {
     it('will return a new string with the number of each character next to it', () => {
-      const result = stringCompression("car");
-      expect(result).toEqual("c1a1r1");
+      const car = stringCompression("car");
+      expect(car).toEqual("c1a1r1");
+    });
+    it('will return a new string with the number of each character next to it', () => {
+      const result = stringCompression("carrrrrrrr");
+      expect(result).toEqual("c1a1r8");
+    });
+    it('will return a new string with the number of each character next to it', () => {
+      const result = stringCompression("plumbus");
+      expect(result).toEqual("p1l1u1m1b1u1s1");
+    });
+    it('will return a new string with the number of each character next to it', () => {
+      const result = stringCompression("access");
+      expect(result).toEqual("a1c2e1s2");
     });
   });
+
+
 });
