3dacf62e295152333ae7471832e2119dc3191cb4
DanielKell <danielkell.ubc@gmail.com>
2017-11-16 14:26:33 -0800

    Refactored function and added new test

--- __tests__/uniqueChar.js
+++ __tests__/uniqueChar.js
@@ -6,8 +6,16 @@ describe('uniqueChar', () => {
 
   describe('when you input a string of letters', () => {
     it('will return the first unique letter', () => {
-      const result = uniqueChar("aaaabbbccd");
+      const result = uniqueChar("aaaabbbd");
       expect(result).toEqual("d");
     });
   });
+
+  describe('when there are no unique characters', () => {
+    it('will return undefined', () => {
+      const result = uniqueChar("aaaaa");
+      expect(result).toEqual(undefined);
+    });
+  });
+
 });
